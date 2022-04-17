0974245471fc4913441eadf1cc824970fc5f5eca
DineshNarlakanti <89600454+DineshNarlakanti@users.noreply.github.com>
2022-04-17 01:07:47 -0500

    Update Ctest.java

--- src/test/java/Ctest.java
+++ src/test/java/Ctest.java
@@ -5,65 +5,42 @@ import static org.junit.Assert.*;
 public class
 Ctest {
     @Test(timeout = 4000)
-    public void test00() throws Throwable {
-        AVLTree avt3 = new AVLTree();
-        AVLTree.Node n1 = avt3.find((int) '4');
-        AVLTree.Node n2 = avt3.getRoot();
-        avt3.delete(103);
-        avt3.insert((int) (byte) 13);
-        AVLTree.Node n4 = avt3.getRoot();
-        avt3.insert((int) (int) 3);
-
+    public void test12() throws Throwable {
         try {
-            avt3.delete(10);
-            fail("NPE");
+            AVLTree aVLTree0 = new AVLTree();
+            int int0 = 1;
+            aVLTree0.delete(1);
+            aVLTree0.insert(1);
+            AVLTree.Node aVLTree_Node0 = aVLTree0.find(1);
+            aVLTree_Node0.height = 1;
+            aVLTree_Node0.height = (-668);
+            aVLTree0.delete(111);
+            aVLTree0.delete((-668));
+            aVLTree_Node0.height = 1;
+            aVLTree0.delete(1487);
+            aVLTree0.height();
+            int int1 = aVLTree0.getBalance(aVLTree_Node0);
+            aVLTree_Node0.key = 1065;
+            AVLTree.Node aVLTree_Node1 = aVLTree0.getRoot();
+            aVLTree_Node0.right = aVLTree_Node1;
+            aVLTree0.delete(0);
+            aVLTree0.insert(int0);
+            int int2 = 0;
+            aVLTree0.insert(int2);
+            AVLTree.Node aVLTree_Node2 = aVLTree0.getRoot();
+            aVLTree0.find(int1);
+            int int3 = 0;
+            aVLTree_Node2.key = int3;
+            aVLTree0.height();
+            aVLTree0.getBalance(aVLTree_Node2);
+            int int4 = 5;
+            AVLTree.Node aVLTree_Node3 = aVLTree0.find(int4);
+            aVLTree_Node3.right = aVLTree_Node2;
+            aVLTree_Node3.key = int3;
+            aVLTree0.getBalance(aVLTree_Node3);
+            aVLTree0.height();
+            aVLTree0.getRoot();
+        } catch (Error error) {
+            fail("fail");
         }
-        catch(NullPointerException ignored){
-        }
-    }
-
-    @Test(timeout = 4000)
-    public void test01() throws Throwable {
-        try {
-            AVLTree sdf = new AVLTree();
-            sdf.insert((-3243));
-            sdf.insert(1776);
-            sdf.delete(645);
-            sdf.insert(3);
-            sdf.insert((-795));
-            sdf.delete((-795));
-            AVLTree.Node nd = sdf.getRoot();
-            sdf.delete((-937));
-            AVLTree.Node node1 = nd.left;
-            nd.left = node1;
-            sdf.insert((-453));
-            nd.left.key = (-1468);
-
-            node1.height = (-937);
-            nd.height = 663;
-            nd.key = (-795);
-            sdf.insert(390);
-            sdf.getBalance(nd);
-        }
-        catch(NullPointerException ne){
-            fail("NPE");
-        }
-    }
-
-    @Test(timeout = 4200)
-    public void test02() throws Throwable {
-        AVLTree aVLTREE = new AVLTree();
-        aVLTREE.insert(13);
-        aVLTREE.insert(57);
-        aVLTREE.insert(26);
-        aVLTREE.insert(84);
-        try {
-            aVLTREE.delete(57);
-            aVLTREE.delete(26);
-            aVLTREE.delete(84);
-            fail("NPE");
-        }catch(NullPointerException ignored){
-
-        }
-    }
-}
+    }}
